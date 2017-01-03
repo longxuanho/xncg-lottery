7acc393a453c9afd602d91b118c27a21c553e059
Long Hồ <Long H>
2017-01-03 20:43:49 +0700

    adding new background

--- src/app/+lottery/lottery.component.ts
+++ src/app/+lottery/lottery.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { LotterySettingsService } from '../shared/lottery-settings.service';
+import { LotterySettings } from '../shared/lottery-settings.model';
+import { ResultService } from '../shared/result.service';
+import { Result, Prizes } from '../shared/result.model';
+import { ToastrService } from 'toastr-ng2';
 
 declare var $: any;
 
@@ -9,17 +15,45 @@ declare var $: any;
 })
 export class LotteryComponent implements OnInit, OnDestroy, AfterViewInit {
 
+  lotterySettings: LotterySettings;
+  subscriptions: {
+    lotterySettings?: Subscription,
+  } = {}
+
   constructor(
+    private lotterySettingsService: LotterySettingsService,
+    private resultService: ResultService,
+    private toastrService: ToastrService,
   ) { }
+
+  handleError(error: Error) {
+    // console.log(`${error.message}: ${error.stack}`);
+    // this.toastrService.error(error.message, 'Ngắt kết nối tới server...');
+    this.subscriptions.lotterySettings.unsubscribe();
+  }
 
 
   ngOnInit() {  
+    this.subscriptions.lotterySettings = this.lotterySettingsService.getSettings()
+      .subscribe(
+      (value: LotterySettings) => {
+        this.lotterySettings = value;
+        if (this.lotterySettings) {
+          $('body').removeClass('has-background-image-1 has-background-image-2 has-background-image-3 has-background-image-4 has-background-image-5 has-background-image-6');
+          $('body').addClass(this.lotterySettings.displayBackgroundImage);
+        }
+      },
+      error => this.handleError(error));
   }
+  
 
   ngAfterViewInit() {
   }
 
   ngOnDestroy() {
+    if (this.lotterySettings)
+      $('body').removeClass('has-background-image-1 has-background-image-2 has-background-image-3 has-background-image-4 has-background-image-5 has-background-image-6');
+    this.subscriptions.lotterySettings.unsubscribe();
   }
 
 
